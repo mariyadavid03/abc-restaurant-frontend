52885f11e3a233ca9d6c3e35f6e269938bf86345
Mariya David <mariyadavidkandy@gmail.com>
2024-08-23 22:21:38 +0530

    feat: Add offer view for customer through carousel slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,58 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import SampleImage from '../../assets/images/sample.jpg';
+import axios from 'axios';
 import './Slider.css';
 
 function Slider() {
+  const [offers, setOffers] = useState([]);
+
+  useEffect(() => {
+
+    // Fetch the offers from your backend
+    axios.get('http://localhost:8080/offer') 
+      .then(response => {
+        setOffers(response.data);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the offers!", error);
+      });
+  }, []);
+
   return (
-    <Carousel className="custom-carousel">
-      <Carousel.Item>
-        <div className="carousel-content">
-          <div className="carousel-image">
-            <img
-              className="d-block w-100"
-              src={SampleImage}
-              alt="First slide"
-            />
+    <Carousel className="custom-carousel" interval={5000} pause={false}>
+      {offers.map((offer, index) => (
+        <Carousel.Item key={index}>
+          <div className="carousel-content">
+            <div className="carousel-image">
+              <img
+                className="d-block w-100"
+                src={`data:image/jpeg;base64,${offer.offer_image_data}`}
+                alt={`Slide ${index + 1}`}
+              />
+            </div>
+            <div className="carousel-text">
+              <h3>{offer.offer_name}</h3>
+              <p>{offer.offer_desc}</p>
+              <p className='discount-rate'>{`Up to ${offer.discount}%`}</p>
+              <p className='valid-till-date'>{`Valid Till ${offer.valid_period}`}</p>
+            </div>
           </div>
-          <div className="carousel-text">
-            <h3>Special Offer</h3>
-            <p>Enjoy a special discount on our gourmet dishes. Elevating culinary standards!</p>
-            <p className='discount-rate'>Up to 20%</p>
-            <p className='valid-till-date'>Valid Till August 31st</p>
-          </div>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="carousel-content">
-          <div className="carousel-image">
-            <img
-              className="d-block w-100"
-              src={SampleImage}
-              alt="Second slide"
-            />
-          </div>
-          <div className="carousel-text">
-            <h3>Exclusive Deal</h3>
-            <p>Join us this weekend for a culinary journey like no other.</p>
-          </div>
-        </div>
-      </Carousel.Item>
-      <Carousel.Item>
-        <div className="carousel-content">
-          <div className="carousel-image">
-            <img
-              className="d-block w-100"
-              src={SampleImage}
-              alt="Third slide"
-            />
-          </div>
-          <div className="carousel-text">
-            <h3>Special Discount</h3>
-            <p>Celebrate with us and enjoy a special discount on our signature dishes.</p>
-          </div>
-        </div>
-      </Carousel.Item>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
